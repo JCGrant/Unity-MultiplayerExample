74ea339e30c8f28f111b60fff490c98616072aed
JCGrant <jamescolin.grant@gmail.com>
2016-08-04 00:02:04 +0100

    Bullets now damage players. Health is not shown yet.

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -2,7 +2,17 @@
 
 public class Bullet : MonoBehaviour {
 
-    void OnCollisionEnter() {
+    private int bulletDamage = 10;
+
+    void OnCollisionEnter(Collision collision) {
+        var hit = collision.gameObject;
+        var health = hit.GetComponent<Health>();
+
+        // Check for null since not all gameObjects have the Health script
+        if (health != null) {
+            health.TakeDamage(bulletDamage);
+        }
+
         Destroy(gameObject);
     }
 
---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Health : MonoBehaviour {
+    
+    public const int maxHealth = 100;
+    public int currentHealth = maxHealth;
+
+    public void TakeDamage(int amount) {
+        currentHealth -= amount;
+        if (currentHealth <= 0) {
+            currentHealth = 0;
+            Debug.Log("Dead!");
+        }
+    }
+
+}
