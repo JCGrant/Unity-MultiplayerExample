01fd8f484c4f21ac3d83373adbf3df2cf24fa2c2
JCGrant <jamescolin.grant@gmail.com>
2016-08-03 22:29:02 +0100

    Add Main scene to Build and add *.app to .gitignore.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,11 +1,16 @@
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : NetworkBehaviour {
 
     private float playerRotateSpeed = 150.0f;
     private float playerMoveSpeed = 3.0f;
 
     void Update() {
+        if (!isLocalPlayer) {
+            return;
+        }
+
         var x = Input.GetAxis("Horizontal") * Time.deltaTime * playerRotateSpeed;
         var z = Input.GetAxis("Vertical") * Time.deltaTime * playerMoveSpeed;
 
