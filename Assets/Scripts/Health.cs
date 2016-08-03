8efe9ccea76ff87eb1bc2fb6e1ad3fbb2d2618a1
JCGrant <jamescolin.grant@gmail.com>
2016-08-04 00:47:10 +0100

    Synchronise player health bars across server and clients.

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,20 +1,27 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Networking;
 
-public class Health : MonoBehaviour {
+public class Health : NetworkBehaviour {
 
     public RectTransform healthBar;
-    
+
     public const int maxHealth = 100;
+    [SyncVar(hook = "OnChangeHealth")]
     public int currentHealth = maxHealth;
 
     public void TakeDamage(int amount) {
+        if (!isServer) {
+            return;
+        }
         currentHealth -= amount;
         if (currentHealth <= 0) {
             currentHealth = 0;
             Debug.Log("Dead!");
         }
-        healthBar.sizeDelta = new Vector2(currentHealth, healthBar.sizeDelta.y);
     }
 
+    void OnChangeHealth(int health) {
+        healthBar.sizeDelta = new Vector2(health, healthBar.sizeDelta.y);
+    }
 }
